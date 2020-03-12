2646881d4cfef0ef9696230f135c83f43d5bc283
Jun Tsai <jcai@ganshane.com>
2020-03-12 18:00:45 +0800

    [build] 1.0.17

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
 import store from './store'
 import config from './config'
 import api from './helpers'
+import * as wxApi from './api/wx'
 
 export class UserNotAuthorized {}
 export {
   config,
   store,
-  api
+  api,
+  wxApi
 }
 
 export * from './api'
